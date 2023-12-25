a38bb81eca8ebb7ac9be11636005dd2ef626c5f8
qinhongjie <hongjie.qin@spacemit.com>
2023-12-25 12:28:57 +0800

    update envsetup.sh for 'source' in anywhere
    Change-Id: I20f67fb28808d1d0ba8dde79ac08a4a71ea72b8f

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#set -e 
-export BIANBU_LINUX_ROOT_DIR=$PWD
+#set -e
+export BIANBU_LINUX_ROOT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))/..
 m() {
     make -C $BIANBU_LINUX_ROOT_DIR  $@
 }
