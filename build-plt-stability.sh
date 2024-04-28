48295b230f203c2555048f1abfc55e24eefbf086
James Deng <james.deng@spacemit.com>
2024-04-28 17:45:14 +0800

    Added build plt stability firmware script
    Change-Id: Ia7612ad0a0c53e35951a527f3d3d07a3857255f9

---
+++ build-plt-stability.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+export BIANBU_LINUX_ARCHIVE=bianbu-linux-k1_plt-stability
+
+WESTON_INI=buildroot-ext/board/spacemit/k1/plt_overlay/etc/xdg/weston/weston.ini
+
+if [ -f $WESTON_INI ]
+then
+    sed -i 's/gui-main/stability/' $WESTON_INI
+    make
+else
+    echo 'Missing $WESTON_INI'
+    exit -1
+fi
